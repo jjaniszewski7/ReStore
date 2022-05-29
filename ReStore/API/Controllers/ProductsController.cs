0110e61aadd15dd728c58809b3a1c206cab157f0
Jakub Janiszewski <jakub.janiszewski7@outlook.com>
2022-05-29 15:51:38 +0200

    Create project ReStore

---
+++ ReStore/API/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Data;
+using API.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ProductsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts() 
+            => await _context.Products.ToListAsync();
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id) 
+            => await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+    }
+}
